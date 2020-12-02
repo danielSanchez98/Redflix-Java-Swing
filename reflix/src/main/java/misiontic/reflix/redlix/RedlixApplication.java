af0d5fbccfdc71f00aecd0c01ddf483c1e8b1cf3
DanielSanchez98 <ds980220@gmail.com>
2020-12-02 00:48:08 -0500

    interfaz basica inicial Spring-Swing

---
+++ reflix/src/main/java/misiontic/reflix/redlix/RedlixApplication.java
@@ -0,0 +1,13 @@
+package misiontic.reflix.redlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedlixApplication.class, args);
+	}
+
+}
